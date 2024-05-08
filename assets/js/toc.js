bb64c6e65193e2d43f86035a78365fdb85fe18d3
Aaron McGinn <amcginn_37@hotmail.com>
2024-05-07 21:57:25 -0500

    minimize jquery; fix links

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,31 +1,30 @@
-// parse page for hTags and create an unordered list automatically. 
-$(function() {
-  let topLevel= 2; // the h-level to start parsing
-  let botLevel = 6; // the h-level to end parsing
+document.addEventListener("DOMContentLoaded", () => {
+  let topLevel= 2; // h-level to start parsing
+  let botLevel = 6;  // h-level to end parsing
   let hTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
   let prev = '';
-  let ulTree = [] // track nested lists to return to the correct level
-  let root = $('.toc'); // element where the TOC is displayed
-  let rootUl = $('<ul>');
+  let ulTree = [] 
+  let root = document.querySelector(".toc"); 
+  let rootUl = document.createElement('ul');
 
   root.append(rootUl);
   ulTree.push(rootUl);
 
-  $(hTags.slice(topLevel - 1, botLevel).join(', section ')).each(function(){
+  document.querySelectorAll(hTags.slice(topLevel - 1, botLevel).join(', section ')).forEach((hEl) => {
 
-    let compare = prev.localeCompare(this.nodeName.toLowerCase());
-    prev = this.nodeName.toLowerCase();
+    let compare = prev.localeCompare(hEl.tagName.toLowerCase());
+    prev = hEl.tagName.toLowerCase();
 
     if (compare < 0) {
       let li = null;
       if (ulTree.at(-1).lastChild) {
         li = ulTree.at(-1).lastChild;
       } else {
-        li = $('<li>');
+        li = document.createElement('li');
         ulTree.at(-1).append(li);
       }
 
-      ul = $('<ul>')
+      ul = document.createElement('ul')
       li.append(ul);
       ulTree.push(ul);
     } else if (compare > 0) {
@@ -33,11 +32,10 @@ $(function() {
     }
 
     let ulNode = ulTree.at(-1);
-    let thisLi = $('<li>');
-    let a = $('<a>', {
-      text: $(this).text(),
-      href: '#' + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'')
-    });
+    let thisLi = document.createElement('li');
+    let a = document.createElement('a')
+    a.textContent = hEl.innerText;
+    a.href = '#' + hEl.innerText.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'');
 
     thisLi.append(a);
 
