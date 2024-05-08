6e392fb2caa4766ff716640e232ac954e55c6206
Aaron McGinn <amcginn_37@hotmail.com>
2024-05-08 09:36:26 -0500

    Update character-img-lazy.js to execute immediately

--- assets/js/character-img-lazy.js
+++ assets/js/character-img-lazy.js
@@ -1,10 +1,8 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  let imgs = document.querySelectorAll('.character .item img');
+let imgs = document.querySelectorAll('.character .item img');
 
-  imgs.forEach((img) => {
-    let src = img.src;
-    img.src = '';
-    img.setAttribute('loading', 'lazy');
-    img.src = src;
-  });
+imgs.forEach((img) => {
+  let src = img.src;
+  img.src = '';
+  img.setAttribute('loading', 'lazy');
+  img.src = src;
 });
